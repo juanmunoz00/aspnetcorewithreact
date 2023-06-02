81c675d28fc568df196418c9e012f0f96f2659c0
Juan Munoz <j.munoz@lacroix.group>
2023-06-02 13:56:29 -0600

    useState done !!

--- ClientApp/src/components/MyComponents/MyFunctionalComponent.js
+++ ClientApp/src/components/MyComponents/MyFunctionalComponent.js
@@ -1,26 +1,45 @@
-﻿import React, { useEffect } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 
 export default function Hello(props) {
 
-    function componentMount() {
-        alert("Component rendered");
-    }
+    /* COMPONENT LYFECYCLE */
+    //function componentMount() {
+    //    alert("Component rendered");
+    //}
 
-    function componentUnmount() {
-        alert("Leaving the component");
-    }
+    //function componentUnmount() {
+    //    alert("Leaving the component");
+    //}
 
-    useEffect(() => {
-        componentMount();
-        return () => {
-            componentUnmount();
-        }
+    //useEffect(() => {
+    //    componentMount();
+    //    return () => {
+    //        componentUnmount();
+    //    }
 
-    }, [])
+    //}, [])
+
+    /* STATE */
+    /* `age` is the state
+     * `setAge` is the method of the state
+     * `useState initilizes the state`
+     * */
+
+    const [age, setAge] = useState(20);
 
     return (
         <div>
-            Hello there, { props.name ? props.name : "John" } !!
+            {/*PROPS*/}
+            <h2>Hello there, {props.name ? props.name : "John"} !!</h2>
+
+            {/*STATE*/}
+            <hr />
+            <h3>State</h3>
+            <span>
+                <b>Age: </b>{age}
+                <button onClick={() => setAge(age + 1)}> + </button>
+                <button onClick={() => setAge(age - 1)}> - </button>                
+            </span>
         </div>
     );
 }
