d2ed3fc49bd69de6f5357da4481628db254a9bae
Juan Munoz <j.munoz@lacroix.group>
2023-06-02 15:17:09 -0600

    Event handling done!!

--- ClientApp/src/components/MyComponents/MyFunctionalComponent.js
+++ ClientApp/src/components/MyComponents/MyFunctionalComponent.js
@@ -27,19 +27,44 @@ export default function Hello(props) {
 
     const [age, setAge] = useState(20);
 
+    /* EVENT HANDLING */
+    /* The event handling is a method that will be executed when `something` hapens on the displayed page like a click.
+     *  
+     * */
+
+    const onChangeAgeInput = (event) => {
+        alert("Age has changed");
+        setAge(parseInt(event.target.value));
+    }
+
+    const showDetails = (prTelephone) => {
+        let name = props.name ? props.name : "John";
+        alert('Name: ' + name + ' | Age:' + age + ' | Telephone: ' + prTelephone);
+    }
+
     return (
         <div>
-            {/*PROPS*/}
+            {/* PROPS */}
             <h2>Hello there, {props.name ? props.name : "John"} !!</h2>
 
-            {/*STATE*/}
+            {/* STATE */}
             <hr />
             <h3>State</h3>
             <span>
-                <b>Age: </b>{age}
+                <b>Age: </b><input type="number" value={age} onChange={ onChangeAgeInput}/>
                 <button onClick={() => setAge(age + 1)}> + </button>
                 <button onClick={() => setAge(age - 1)}> - </button>                
             </span>
+
+            { /* EVENT HANDLING */}
+            <hr />
+            <h3>Event Handling</h3>
+            <span>
+                <b>Age: </b>{age}
+                <button onClick={showDetails.bind(this, 123456) }> Show Details </button>
+                
+            </span>
+
         </div>
     );
 }
